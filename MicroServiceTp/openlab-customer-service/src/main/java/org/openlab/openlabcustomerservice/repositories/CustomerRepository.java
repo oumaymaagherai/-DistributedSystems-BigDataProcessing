96faa2460f0339c14fa52a06a6d440a46435debc
oumaymaagherai <agherai06@gmail.com>
2022-12-24 14:35:18 +0100

    modif

--- MicroServiceTp/openlab-customer-service/src/main/java/org/openlab/openlabcustomerservice/repositories/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.openlab.openlabcustomerservice.repositories;
-
-import org.openlab.openlabcustomerservice.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,String> {
-    Customer getById(String id);
-}
